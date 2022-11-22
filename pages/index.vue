3a975a5b0cb9b9901ec05eab8d5d02dc66ba64d3
Sven <svenrisse0@gmail.com>
2022-11-22 16:11:38 +0100

    feat: added content to index

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,21 @@
 <template>
-    <NuxtLink to="/about">About</NuxtLink>
-    <NuxtLink to="/projects">Projects</NuxtLink>
-    <NuxtLink to="/work">Work</NuxtLink>
-    <NuxtLink to="/contact">Contact</NuxtLink>
+    <div>
+        <nav>
+            <NuxtLink to="/about">About</NuxtLink>
+            <NuxtLink to="/projects">Projects</NuxtLink>
+            <NuxtLink to="/work">Work</NuxtLink>
+            <NuxtLink to="/contact">Contact</NuxtLink>
+        </nav>
+        <div class="introduction">
+            <span>Hi, my name is </span>
+            <h1>Sven Risse.</h1>
+            <h1>I'm an aspiring fullstack webdev.</h1>
+            <p>
+                I'm a software engineer, with a passion for new and exiting
+                technologies and frameworks. Currently, I'm focused on 
+                diving deep into Frontend Development with Vue and Nuxt.
+
+            </p>
+        </div>
+    </div>
 </template>
