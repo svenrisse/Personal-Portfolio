9c42cd14abecc2a349507d4a985ffce9bc51a93b
Sven <svenrisse0@gmail.com>
2022-11-23 08:39:19 +0100

    feat: add Navbar component

---
+++ components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav class="navbar">
+        <NuxtLink class="navbar-link" to="/about">About</NuxtLink>
+        <NuxtLink class="navbar-link" to="/projects">Projects</NuxtLink>
+        <NuxtLink class="navbar-link" to="/work">Work</NuxtLink>
+        <NuxtLink class="navbar-link" to="/contact">Contact</NuxtLink>
+    </nav>
+</template>
+
+<style>
+    .navbar {
+        position: fixed;
+        right: 4vw;
+        font-family: var(--font-mono);
+    }
+
+    .navbar-link {
+        margin-left: 2vw;
+        text-decoration: none;
+        color: var(--lightest-slate);
+    }
+    .navbar-link:hover {
+        color: var(--vue-green);
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,6 @@
 <template>
     <div>
-        <nav>
-            <NuxtLink to="/about">About</NuxtLink>
-            <NuxtLink to="/projects">Projects</NuxtLink>
-            <NuxtLink to="/work">Work</NuxtLink>
-            <NuxtLink to="/contact">Contact</NuxtLink>
-        </nav>
+        <Navbar />
         <div class="introduction">
             <span>Hi, my name is </span>
             <h1>Sven Risse.</h1>
