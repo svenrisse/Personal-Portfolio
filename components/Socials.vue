2af3f5a84700cf1759d79b3dec3e36e1183c0a8d
Sven <svenrisse0@gmail.com>
2022-11-23 12:02:37 +0100

    feat: add icons for socials

--- components/Socials.vue
+++ components/Socials.vue
@@ -1,19 +1,24 @@
 <template>
     <div class="socials">
-        <a href="https://github.com/svenrisse"
-            ><img src="../assets/images/github.png" alt="Github Icon"
-        /></a>
-        <a href="https://www.linkedin.com/in/sven-risse-3b2262251/"
-            ><img src="../assets/images/linkedin.png" alt="Linkedin Icon"
-        /></a>
+        <a href="https://github.com/svenrisse" class="socials-icon">
+            <Icon name="uil:github" size="2.5rem" />
+        </a>
+        <a href="https://www.linkedin.com/in/sven-risse-3b2262251/">
+            <Icon name="uil:linkedin" size="2.5rem" class="socials-icon" />
+        </a>
+        <a href="mailto:sven@srisse.dev">
+            <Icon name="material-symbols:mail-rounded" size="2.5rem" class="socials-icon" />
+        </a>
     </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style>
-img {
-    height: 3vh;
-    width: 2vw;
+<style lang="scss">
+a {
+    color: var(--white);
+    &:hover {
+        color: var(--vue-green);
+    }
 }
 </style>
