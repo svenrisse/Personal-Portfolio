fcf21eaad6b2993becbfb212d121bc66c6021d42
Sven <svenrisse0@gmail.com>
2022-11-24 15:38:52 +0100

    style: add styling for main content on index page

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,10 @@
         <Logo />
         <Navbar />
         <div class="app-main">
-            <span>Hi, my name is </span>
+            <span class="intro-greeting">Hi, my name is </span>
             <h1 class="intro-name">Sven Risse.</h1>
-            <h1>I'm an aspiring fullstack webdev.</h1>
-            <p>
+            <h1 class="intro-description">I'm an aspiring  <span id="intro-description__job"> fullstack web developer</span>.</h1>
+            <p class="intro-paragraph">
                 I'm a software engineer, with a passion for new and exiting
                 technologies and frameworks. <br />
                 Currently, I'm focused on diving deep into Frontend Development
@@ -19,13 +19,35 @@
 </template>
 
 <style lang="scss">
+.intro-greeting {
+    font-size: 24px;
+    color: var(--light-slate);
+}
+
 .intro-name {
     max-width: fit-content;
+    margin-top: 2vh;
+    margin-bottom: 2vh;
+    font-size: 70px;
+    color: var(--vue-green);
     border-right: 3px solid var(--vue-green);
     white-space: nowrap;
     overflow: hidden;
     animation: typewriter 4s steps(44) 1s 1 normal both,
         blinkTextCursor 500ms steps(44) infinite normal;
+}
+
+.intro-description {
+    font-size: 70px;
+}
+
+#intro-description__job {
+    color: var(--vue-green);
+}
+
+.intro-paragraph {
+    font-size: 30px;
+    color: var(--lightest-slate);
 }
 
 @keyframes typewriter {
