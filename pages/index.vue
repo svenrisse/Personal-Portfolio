3a7846c5b0d6c7a5d69b98016191ac25f9afe1a3
Sven <svenrisse0@gmail.com>
2022-11-24 08:10:49 +0100

    style: move main content to center

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
+    <div class="app">
         <Navbar />
-        <div class="introduction">
+        <div class="app-main">
             <span>Hi, my name is </span>
             <h1>Sven Risse.</h1>
             <h1>I'm an aspiring fullstack webdev.</h1>
             <p>
                 I'm a software engineer, with a passion for new and exiting
-                technologies and frameworks. Currently, I'm focused on 
+                technologies and frameworks. <br> Currently, I'm focused on 
                 diving deep into Frontend Development with Vue and Nuxt.
 
             </p>
