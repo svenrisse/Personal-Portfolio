2f1198f07b13b40253e60ae32dd0fe07945b9883
Sven <svenrisse0@gmail.com>
2022-11-25 12:32:48 +0100

    feat: add work in progress info on empty pages

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,13 @@
 <template>
-    <Logo />
-    <Navbar />
-    <div class="app-main">
-
+    <div class="app">
+        <Logo />
+        <Navbar />
+        <div class="app-main">
+            <h1>
+                This is still Work in Progress. Please come back later :)
+            </h1> 
+        </div>
+        <Socials />
+        <Mail />
     </div>
-    <Socials />
-    <Mail />
 </template>
