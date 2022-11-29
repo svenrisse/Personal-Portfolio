8d4860271ff250f7e9a9525e702f5eca49d8c845
Sven <svenrisse0@gmail.com>
2022-11-29 19:45:53 +0100

    feat: add title and icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: [
-        "@/assets/css/main.scss"
-    ],
+    css: ["@/assets/css/main.scss"],
 
-    modules: [
-        "nuxt-icon"
-    ]
-})
+    modules: ["nuxt-icon"],
+
+    app: {
+        head: {
+            title: "Sven Risse",
+            link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+        }
+    }
+});
