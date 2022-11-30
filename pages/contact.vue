c6a8e6ca9f5cee39c477df8458b19cb1605b5223
Sven <svenrisse0@gmail.com>
2022-11-30 20:36:30 +0100

    feat<contact>:  add content and style

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,11 +3,31 @@
         <Logo />
         <Navbar />
         <div class="app-main">
-            <h1>
-                This is still Work in Progress. Please come back later :)
-            </h1> 
+            <h1 class="contact-header">Contact</h1>
+            <p class="contact-content">
+                As I am currently in training, I'm not actively looking for new
+                opportunities, but nevertheless feel free to contact me anytime
+                on my socials or my mail,
+                <a href="mailto:sven@srisse.dev" id="contact-mail">sven@srisse.dev</a>
+            </p>
         </div>
         <Socials />
         <Mail />
     </div>
 </template>
+
+<style lang="scss">
+    .contact-header {
+        text-align: center;
+        font-family: var(--font-mono);
+        font-size: 2.5rem;
+    }
+
+    .contact-content {
+        font-size: 2rem;
+    }
+    
+    #contact-mail {
+        color: var(--vue-green);
+    }
+</style>
