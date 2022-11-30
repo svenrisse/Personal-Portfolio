9f673aa45eec787ea60a2b06419414aedbbb2ce4
Sven <svenrisse0@gmail.com>
2022-11-30 22:27:24 +0100

    feat<projects>: add ProjectExample component, add first project

---
+++ components/ProjectExample.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+defineProps<{
+    imageLink: String;
+    projectLink: String;
+    title: String;
+    description: String;
+    technologies: String;
+    githubLink: String;
+}>();
+</script>
+
+<template>
+    <div class="project-example">
+        <a :href="`${projectLink}`">
+            <img :src="`/img/${imageLink}`" />
+        </a>
+        <div class="project-text">
+            <h1> {{ title }} </h1>
+            <p> {{description}} </p>
+            <span> {{ technologies }}</span>
+            <a :href="`https://www.github.com/svenrisse/${githubLink}`" class="socials-icon" title="Github">
+            <br />
+            <Icon name="mdi:github" size="2.5rem" />
+        </a>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+.project-example {
+    display: flex;
+    img {
+        height: 20vh;
+        padding: 1rem;
+        border: 1px solid var(--vue-green);
+        border-radius: 10px;
+    }
+
+    .project-text {
+        margin-left: 2vw;
+    }
+}
+</style>
--- pages/projects.vue
+++ pages/projects.vue
@@ -3,9 +3,10 @@
         <Logo />
         <Navbar />
         <div class="app-main">
-            <h1>
-                This is still Work in Progress. Please come back later :)
+            <h1 class="projects-header">
+                Some Projects I've built
             </h1> 
+            <ProjectExample imageLink="nuxt-portfolio.png" projectLink="https://www.srisse.dev/" title="Personal Portfolio" description="You are on it right now! I learned some Nuxt basics with this project while also deepening my understanding of Vue." technologies="Nuxt Vue SCSS" githubLink="Personal-Portfolio"/> 
         </div>
         <Socials />
         <Mail />
