294c33aa15442061ceb56eb4bc552743ed2f63b4
Sven <svenrisse0@gmail.com>
2022-11-30 23:01:19 +0100

    fix<contact>: add margin and width from app-main

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,13 +2,15 @@
     <div class="app">
         <Logo />
         <Navbar />
-        <div class="app-main">
+        <div class="app-contact">
             <h1 class="contact-header">Contact</h1>
             <p class="contact-content">
                 As I am currently in training, I'm not actively looking for new
                 opportunities, but nevertheless feel free to contact me anytime
                 on my socials or my mail,
-                <a href="mailto:sven@srisse.dev" id="contact-mail">sven@srisse.dev</a>
+                <a href="mailto:sven@srisse.dev" id="contact-mail"
+                    >sven@srisse.dev</a
+                >
             </p>
         </div>
         <Socials />
@@ -17,17 +19,21 @@
 </template>
 
 <style lang="scss">
-    .contact-header {
-        text-align: center;
-        font-family: var(--font-mono);
-        font-size: 2.5rem;
-    }
+.app-contact {
+    width: 50vw;
+    margin-top: 30vh;
+}
+.contact-header {
+    text-align: center;
+    font-family: var(--font-mono);
+    font-size: 2.5rem;
+}
 
-    .contact-content {
-        font-size: 2rem;
-    }
-    
-    #contact-mail {
-        color: var(--vue-green);
-    }
+.contact-content {
+    font-size: 2rem;
+}
+
+#contact-mail {
+    color: var(--vue-green);
+}
 </style>
