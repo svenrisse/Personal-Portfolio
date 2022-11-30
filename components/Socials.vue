ea13f6ead6b0b29b6bd892ae20624e4b393cdd4e
Sven <svenrisse0@gmail.com>
2022-11-30 21:22:42 +0100

    feat<socials>: add title to links

--- components/Socials.vue
+++ components/Socials.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="socials">
-        <a href="https://github.com/svenrisse" class="socials-icon">
+        <a href="https://github.com/svenrisse" class="socials-icon" title="Github">
             <Icon name="mdi:github-box" size="2.5rem" />
         </a>
-        <a href="https://www.linkedin.com/in/sven-risse-3b2262251/">
+        <a href="https://www.linkedin.com/in/sven-risse-3b2262251/" title="Linkedin">
             <Icon name="uil:linkedin" size="2.5rem" class="socials-icon" />
         </a>
-        <a href="https://www.xing.com/profile/Sven_Risse2/cv">
+        <a href="https://www.xing.com/profile/Sven_Risse2/cv" title="Xing">
             <Icon name="mdi:xing" size="2.5rem" class="socials-icon"/>
         </a>
-        <a href="mailto:sven@srisse.dev">
+        <a href="mailto:sven@srisse.dev" title="Mail">
             <Icon name="material-symbols:mail-rounded" size="2.5rem" class="socials-icon" />
         </a>
     </div>
