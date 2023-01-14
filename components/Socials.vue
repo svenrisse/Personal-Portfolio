8465433c39a4385c21618e8aa76782f95da5409e
Sven <svenrisse0@gmail.com>
2023-01-14 17:27:54 +0100

    feat: update mailto: in socials and on contact page

--- components/Socials.vue
+++ components/Socials.vue
@@ -1,29 +1,35 @@
 <template>
-    <div class="socials">
-        <a href="https://github.com/svenrisse" class="socials-icon" title="Github">
-            <Icon name="mdi:github-box" size="2.5rem" />
-        </a>
-        <a href="https://www.linkedin.com/in/sven-risse-3b2262251/" title="Linkedin">
-            <Icon name="uil:linkedin" size="2.5rem" class="socials-icon" />
-        </a>
-        <a href="https://www.xing.com/profile/Sven_Risse2/cv" title="Xing">
-            <Icon name="mdi:xing" size="2.5rem" class="socials-icon"/>
-        </a>
-        <a href="mailto:sven@srisse.dev" title="Mail">
-            <Icon name="material-symbols:mail-rounded" size="2.5rem" class="socials-icon" />
-        </a>
-    </div>
+  <div class="socials">
+    <a href="https://github.com/svenrisse" class="socials-icon" title="Github">
+      <Icon name="mdi:github-box" size="2.5rem" />
+    </a>
+    <a
+      href="https://www.linkedin.com/in/sven-risse-3b2262251/"
+      title="Linkedin"
+    >
+      <Icon name="uil:linkedin" size="2.5rem" class="socials-icon" />
+    </a>
+    <a href="https://www.xing.com/profile/Sven_Risse2/cv" title="Xing">
+      <Icon name="mdi:xing" size="2.5rem" class="socials-icon" />
+    </a>
+    <a href="mailto:sven@svenrisse.dev" title="Mail">
+      <Icon
+        name="material-symbols:mail-rounded"
+        size="2.5rem"
+        class="socials-icon"
+      />
+    </a>
+  </div>
 </template>
 
 <style lang="scss">
 .socials {
-    position: fixed;
-    left: 3%;
-    bottom: 5%;
-    height: 17.5vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
+  position: fixed;
+  left: 3%;
+  bottom: 5%;
+  height: 17.5vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
-
 </style>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,54 +1,57 @@
 <template>
-    <div class="app">
-        <Logo />
-        <Navbar />
-        <div class="app-contact">
-            <h1 class="contact-header">Contact</h1>
-            <div class="contact-content">
-                <p>
-                    As I am currently in training, I'm not actively looking for new opportunities, but nevertheless feel
-                    free to contact me anytime on my socials or my mail:
-                </p>
-                <a href="mailto:sven@srisse.dev" id="contact-mail">sven@svenrisse.dev</a>
-            </div>
-        </div>
-        <Socials />
-        <Mail />
+  <div class="app">
+    <Logo />
+    <Navbar />
+    <div class="app-contact">
+      <h1 class="contact-header">Contact</h1>
+      <div class="contact-content">
+        <p>
+          As I am currently in training, I'm not actively looking for new
+          opportunities, but nevertheless feel free to contact me anytime on my
+          socials or my mail:
+        </p>
+        <a href="mailto:sven@svenrisse.dev" id="contact-mail"
+          >sven@svenrisse.dev</a
+        >
+      </div>
     </div>
+    <Socials />
+    <Mail />
+  </div>
 </template>
 
 <style lang="scss">
 .app-contact {
-    width: 60vw;
-    margin-top: 30vh;
+  width: 60vw;
+  margin-top: 30vh;
 }
 .contact-header {
-    text-align: center;
-    font-family: var(--font-mono);
-    font-size: 2.5rem;
+  text-align: center;
+  font-family: var(--font-mono);
+  font-size: 2.5rem;
 }
 
 .contact-content {
-    text-align: center;
-    font-size: 1.25rem;
-    -webkit-animation: fadein 3s; /* Safari, Chrome and Opera > 12.1 */
-    -moz-animation: fadein 3s; /* Firefox < 16 */
-    -ms-animation: fadein 3s; /* Internet Explorer */
-    -o-animation: fadein 3s; /* Opera < 12.1 */
-    animation: fadein 3s;
+  text-align: center;
+  font-size: 1.25rem;
+  -webkit-animation: fadein 3s; /* Safari, Chrome and Opera > 12.1 */
+  -moz-animation: fadein 3s; /* Firefox < 16 */
+  -ms-animation: fadein 3s; /* Internet Explorer */
+  -o-animation: fadein 3s; /* Opera < 12.1 */
+  animation: fadein 3s;
 
-    @media screen and (min-width: 640px) {
-        font-size: 1.75rem;
-    }
-    @media screen and (min-width: 1000px) {
-        font-size: 2rem;
-    }
-    @media screen and (min-width: 1500px) {
-        font-size: 2.5rem;
-    }
+  @media screen and (min-width: 640px) {
+    font-size: 1.75rem;
+  }
+  @media screen and (min-width: 1000px) {
+    font-size: 2rem;
+  }
+  @media screen and (min-width: 1500px) {
+    font-size: 2.5rem;
+  }
 }
 
 #contact-mail {
-    color: var(--vue-green);
+  color: var(--vue-green);
 }
 </style>
