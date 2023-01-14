ffc6de145f289cb3b4e120b3b3a56d82e5c131b5
Sven <svenrisse0@gmail.com>
2023-01-14 17:23:52 +0100

    feat: update mailto:

--- components/Mail.vue
+++ components/Mail.vue
@@ -1,18 +1,18 @@
 <template>
-    <a href="mailto:sven@srisse.dev" class="mail-link">sven@srisse.dev</a>
+  <a href="mailto:sven@svenrisse.dev" class="mail-link">sven@svenrisse.dev</a>
 </template>
 
 <style lang="scss">
-    .mail-link {
-        display: none;
-        position: fixed;
-        bottom: 5%;
-        right: 4%;
-        text-decoration: none;
-        writing-mode: vertical-rl;
+.mail-link {
+  display: none;
+  position: fixed;
+  bottom: 5%;
+  right: 4%;
+  text-decoration: none;
+  writing-mode: vertical-rl;
 
-        @media screen and (min-width: 640px) {
-            display: initial;
-        }
-    }
+  @media screen and (min-width: 640px) {
+    display: initial;
+  }
+}
 </style>
