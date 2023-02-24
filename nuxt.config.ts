766277d8be6d34a3c178ca42bce73342acb09bab
Sven <svenrisse0@gmail.com>
2023-02-24 12:25:05 +0100

    feat(meta): update description and add lang

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ["@/assets/css/main.scss"],
+  css: ["@/assets/css/main.scss"],
 
-    modules: ["nuxt-icon"],
+  modules: ["nuxt-icon"],
 
-    app: {
-        head: {
-            title: "Sven Risse",
-            link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-        }
-    }
+  app: {
+    head: {
+      title: "Sven Risse",
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+      meta: [
+        {
+          name: "description",
+          content:
+            "Personal Portfolio of Sven Risse, aspriring fullstack webdeveloper, based in Germany.",
+        },
+      ],
+      htmlAttrs: {
+        lang: "en",
+      },
+    },
+  },
 });
